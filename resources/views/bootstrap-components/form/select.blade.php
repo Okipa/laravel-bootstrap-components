ce8524aa1c2eb2fde089c5b902f438d3b8bca514
Arthur LORENT <arthur@acid.fr>
2019-09-05 12:12:33 +0200

    1.0.2

--- resources/views/bootstrap-components/form/select.blade.php
+++ resources/views/bootstrap-components/form/select.blade.php
@@ -7,8 +7,8 @@
     @endif
         @include('bootstrap-components::bootstrap-components.partials.prepend')
         <select id="{{ $componentId }}" {{ classTag($type . '-' . Str::slug($name) . '-component', 'custom-select', $componentClasses, validationStatus($name)) }}name="{{ $name . ($multiple ? '[]' : '') }}" {{ htmlAttributes($multiple ? 'multiple' : null, $componentHtmlAttributes) }}>
-            @if(! empty($placeholder))
-                <option value="" disabled="disabled"{{ htmlAttributes(count(array_filter(Arr::pluck($options, 'selected'))) ? null : ['selected' => 'selected']) }}>{{ $placeholder }}</option>
+            @if($placeholder)
+                <option value="" disabled="disabled"{{ htmlAttributes(count(array_filter(Arr::pluck($options, 'selected'))) ? null : ['selected' => 'selected']) }}>@lang($placeholder)</option>
             @endif
             @foreach($options as $option)
                 <option value="{{ $option[$optionValueField] }}"{{ htmlAttributes(!empty($option['selected']) && $option['selected'] === true ? ['selected' => 'selected'] : null) }}>{{ $option[$optionLabelField] }}</option>
