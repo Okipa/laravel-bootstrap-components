c51a4be4e7ccd4ce2ca84ca751fb4dac8f384797
Arthur LORENT <arthur@acid.fr>
2019-10-15 16:46:30 +0200

    1.0.8

--- src/ComponentServiceProvider.php
+++ src/ComponentServiceProvider.php
@@ -13,13 +13,13 @@ class ComponentServiceProvider extends ServiceProvider
         $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'bootstrap-components');
         $this->publishes([
             __DIR__ . '/../config/bootstrap-components.php' => config_path('bootstrap-components.php'),
-        ], 'bootstrap-components::config');
+        ], 'bootstrap-components:config');
         $this->publishes([
-            __DIR__ . '/../resources/lang' => resource_path('lang'),
-        ], 'bootstrap-components::translations');
+            __DIR__ . '/../resources/lang' => resource_path('lang/vendor/bootstrap-components'),
+        ], 'bootstrap-components:translations');
         $this->publishes([
             __DIR__ . '/../resources/views' => resource_path('views/vendor/bootstrap-components'),
-        ], 'bootstrap-components::views');
+        ], 'bootstrap-components:views');
         // we load the laravel html helper package
         // https://github.com/Okipa/laravel-html-helper
         $this->app->register(HtmlHelperServiceProvider::class);
