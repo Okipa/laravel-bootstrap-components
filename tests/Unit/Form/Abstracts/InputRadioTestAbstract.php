f41713fcf524d8371f375fd364dcb0c101dfb652
Arthur LORENT <arthur@acid.fr>
2020-03-19 15:44:18 +0100

    wip

--- tests/Unit/Form/Abstracts/InputRadioTestAbstract.php
+++ tests/Unit/Form/Abstracts/InputRadioTestAbstract.php
@@ -134,10 +134,11 @@ abstract class InputRadioTestAbstract extends InputTestAbstract
 
     public function testOldZeroValue()
     {
-        $oldValue = '0';
+        $oldValue = 0;
         $this->app['router']->get('test', [
             'middleware' => 'web', 'uses' => function () use ($oldValue) {
-                $request = request()->merge(['name' => (int) $oldValue]);
+                // http values are always stored as string
+                $request = request()->merge(['name' => (string) $oldValue]);
                 $request->flash();
             },
         ]);
